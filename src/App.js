cd8acacb0bfd3b5aed791de122949100d5ba9db7
gfR616 <616gfr@gmail.com>
2024-03-13 17:40:44 +0500

    начал верстку

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function App() {
-	return 123
-}
-
-export default App
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { TodosContainer } from './components/todos/index.tsx'
+import React from 'react'
+
+// interface IAppProps {
+// 	store: Store
+// 	storePersistor: Persistor
+// }
+
+export const App = () => {
+	return (
+		<>
+			<TodosContainer />
+		</>
+	)
+}
+
+export default App
