c6130915554e91efb23333f7b3a3c35caf56aab9
gfR616 <616gfr@gmail.com>
2024-03-14 15:03:31 +0500

    добавил изменение отображения тасок
    в зависимости от ширины экрана

--- src/components/TodoList/todoList.tsx
+++ src/components/TodoList/todoList.tsx
@@ -1,6 +1,6 @@
 import { List } from 'antd'
 import TodoItem from 'components/todoItem/todoItem'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ITodo } from 'store/todo/models/todo.model'
 
 interface ITodoListProps {
@@ -13,25 +13,47 @@ export const TodoList: React.FC<ITodoListProps> = ({
 	todos,
 	onTodoRemoval,
 	onTodoToggle,
-}) => (
-	<List
-		locale={{
-			emptyText: 'Здесь пока ничего нет :(',
-		}}
-		dataSource={todos}
-		renderItem={(todo) => (
-			<TodoItem
-				key={todo.id}
-				todo={todo}
-				onTodoToggle={onTodoToggle}
-				onTodoRemoval={onTodoRemoval}
-			/>
-		)}
-		pagination={{
-			position: 'bottom',
-			pageSize: 10,
-		}}
-	/>
-)
+}) => {
+	const [pageSize, setPageSize] = useState(5)
+
+	useEffect(() => {
+		const handleResize = () => {
+			const windowWidth = window.innerWidth
+			if (windowWidth <= 600) {
+				setPageSize(3)
+			} else if (windowWidth <= 900) {
+				setPageSize(4)
+			} else {
+				setPageSize(6)
+			}
+		}
+
+		handleResize()
+
+		window.addEventListener('resize', handleResize)
+		return () => window.removeEventListener('resize', handleResize)
+	}, [])
+
+	return (
+		<List
+			locale={{
+				emptyText: 'Здесь пока ничего нет :(',
+			}}
+			dataSource={todos}
+			renderItem={(todo) => (
+				<TodoItem
+					key={todo.id}
+					todo={todo}
+					onTodoToggle={onTodoToggle}
+					onTodoRemoval={onTodoRemoval}
+				/>
+			)}
+			pagination={{
+				position: 'bottom',
+				pageSize: pageSize, // Используйте состояние pageSize
+			}}
+		/>
+	)
+}
 
 export default TodoList
