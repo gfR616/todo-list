0ea2f8c3042e1a114add416659d85b8c4eed9a3a
gfR616 <616gfr@gmail.com>
2024-03-14 20:11:30 +0500

    добавил роутер

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Store } from '@reduxjs/toolkit'
 import { TodosContainer } from 'components/todosContainer/todosContainer'
 import React from 'react'
 import { Provider } from 'react-redux'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { Persistor } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
 
@@ -14,7 +15,11 @@ export const App: React.FC<IAppProps> = ({ store, storePersistor }) => {
 	return (
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={storePersistor}>
-				<TodosContainer />
+				<HashRouter>
+					<Routes>
+						<Route path="/" element={<TodosContainer />} />
+					</Routes>
+				</HashRouter>
 			</PersistGate>
 		</Provider>
 	)
